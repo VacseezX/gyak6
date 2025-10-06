bb07c35b9a7cf2379e9cb5df808a9e9e511bc77f
VacseezX <c2vacger@vasvari.org>
2025-10-06 09:16:55 +0200

    fixed

--- prim_feladat.py
+++ prim_feladat.py
@@ -1,7 +1,16 @@
+import math
 gyartasiSzam = int(input("Add meg a csoki gyártási sorszámát: "))
 tmp = False
 if gyartasiSzam <= 1:
     tmp = False
 else:
-    for i
-    
+    tmp = True
+    for i in range(2, int(math.sqrt(gyartasiSzam)) + 1):
+        if gyartasiSzam % i == 0:
+            tmp = False
+            break
+
+if tmp != False:
+    print("Gratulálok, nyertél!")
+else:
+    print("Sajnos nem nyert!")
